250364a2e5fc4569e3e377e8f558f2fb2d2b921a
Jesse White <jesse.white6@gmail.com>
2021-05-21 14:04:39 -0700

    update background image.

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -23,7 +23,7 @@ namespace HairSalon.Controllers
 
     public ActionResult Create()
     {
-      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name", new { @class = "custom-select" });
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
       return View();
     }
 
@@ -44,7 +44,7 @@ namespace HairSalon.Controllers
     public ActionResult Edit(int id)
     {
       var thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
-      ViewBag.ClientId = new SelectList(_db.Clients, "ClientId", "Name");
+      ViewBag.StylistId = new SelectList(_db.Clients, "StylistId", "Name");
       return View(thisClient);
     }
 
